157abc3315651ae381a0ae594da449fa07d23c4d
Amancore__ <aman8804623822@gmail.com>
2025-06-05 09:20:24 +0530

    refactor: simplify HTML structure in RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,10 +22,8 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body
-				suppressHydrationWarning={true}
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+		<html lang="en" className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+			<body suppressHydrationWarning={true}>
 				{children}
 			</body>
 		</html>
